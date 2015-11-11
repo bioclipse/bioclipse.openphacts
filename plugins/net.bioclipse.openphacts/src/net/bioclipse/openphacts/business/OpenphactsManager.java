b95a28f75850f75bfb16193e19a621eade6b864c
Egon Willighagen <egon.willighagen@gmail.com>
2015-11-11 17:06:34 +0100

    Use the 1.5 API

--- plugins/net.bioclipse.openphacts/src/net/bioclipse/openphacts/business/OpenphactsManager.java
+++ plugins/net.bioclipse.openphacts/src/net/bioclipse/openphacts/business/OpenphactsManager.java
@@ -139,7 +139,7 @@ public class OpenphactsManager implements IBioclipseManager {
 	private String getOPSLDAendpoint(){
 		 IEclipsePreferences preferences = ConfigurationScope.INSTANCE
 				  .getNode(OPENPHACTS_PREFERENCE_NODE);
-		 return preferences.get(OPENPHACTS_ENDPOINT_PREFERENCE,"https://beta.openphacts.org/1.4/");
+		 return preferences.get(OPENPHACTS_ENDPOINT_PREFERENCE,"https://beta.openphacts.org/1.5/");
 	}
 	public void setOPSLDAendpoint(String endpoint){
 		 IEclipsePreferences preferences = ConfigurationScope.INSTANCE
